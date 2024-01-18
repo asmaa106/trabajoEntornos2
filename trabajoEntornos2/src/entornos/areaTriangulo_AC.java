4a3365389aaccf2454f822fd1100670697b9789d
alexandra <alexandra@1DAM-08>
2024-01-18 13:59:11 +0100

    subiendo area triangulo (alexandra)

--- trabajoEntornos2/src/entornos/areaTriangulo_AC.java
+++ trabajoEntornos2/src/entornos/areaTriangulo_AC.java
@@ -1,5 +1,24 @@
 package entornos;
-
+import java.util.Scanner;
 public class areaTriangulo_AC {
+	
+	    public static void main(String[] args) {
+	        Scanner scanner = new Scanner(System.in);
 
-}
+	        System.out.println("Ingrese la longitud del lado 1 del triángulo:");
+	        int lado1 = scanner.nextInt();
+
+	        System.out.println("Ingrese la longitud del lado 2 del triángulo:");
+	        int lado2 = scanner.nextInt();
+
+	        double area = calcularArea(lado1, lado2);
+
+	        System.out.println("El área del triángulo es: " + area);
+	    }
+
+	    public static double calcularArea(int lado1, int lado2) {
+	        return (double) (lado1 * lado2) / 2;
+	    }
+	}
+
+
